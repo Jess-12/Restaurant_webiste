0b4cafb0e0ceefecce12f60791329dad3fddcfaf
Jess-12 <jjessica2404@gmail.com>
2023-05-18 19:48:24 +0530

    gzhgggfz

---
+++ javascript.js
@@ -0,0 +1,11 @@
+let menu = document.querySelector('#menu-bar');
+let menu = document.querySelector('.navbar');
+
+menu.onckick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
